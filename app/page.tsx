9977fb0e688cdd8fa0148a9b1dc5f5a1710e7e48
Jayed Rashid <user@users-MacBook-Air.local>
2023-06-25 20:16:18 +0600

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,23 @@
+import About from "./components/About";
+import Banner from "./components/Banner";
+import Experience from "./components/Experience";
+import Skills from "./components/Skills";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+
+export default function Home() {
+  return (
+    <main className="w-full bg-gray-950 text-gray-300 px-4">
+      <div className="max-w-screen-xl mx-auto lg:pt-10">
+        <Banner />
+        <About />
+        <Experience />
+        <Skills />
+        <Projects />
+        <Contact />
+        <Footer />
+      </div>
+    </main>
+  );
+}
